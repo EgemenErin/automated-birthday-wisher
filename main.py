7d68265f9f574655ede89277c880042355294268
Egemen Erin <pixelsushidev@hotmail.com>
2024-05-18 19:03:57 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+birthdays = pd.read_csv("birthdays.csv")
+
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
